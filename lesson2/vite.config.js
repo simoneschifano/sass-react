4f4946db2cf8f7e4c0efb7ccb6c62db183b245fa
simone schifano <simone.schifano@gmail.com>
2023-07-04 12:44:37 +0200

    Add files via upload

---
+++ lesson2/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/scss/variables/colors.scss";
+          @import "./src/scss/variables/margins.scss";
+          @import "./src/scss/variables/elevations.scss";
+          @import "./src/scss/variables/animations.scss";
+          @import "./src/scss/variables/typography.scss";
+          @import "./src/scss/helpers/extends.scss";
+          @import "./src/scss/helpers/mixins.scss";
+          @import "./src/scss/helpers/devices.scss";
+          @import "./src/scss/helpers/functions.scss";
+        `,
+      },
+    },
+  },
+});
