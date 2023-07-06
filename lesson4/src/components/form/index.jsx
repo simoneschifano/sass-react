d009c09823d2973dc9f65125d025b25de905aadd
simone schifano <simone.schifano@gmail.com>
2023-07-06 12:42:29 +0200

    Add files via upload

---
+++ lesson4/src/components/form/index.jsx
@@ -0,0 +1,7 @@
+import InputToggle from "./InputToggle";
+import InputText from "./InputText";
+import InputPassword from "./InputPassword";
+import InputSelect from "./InputSelect";
+import DropdownSelect from "./DropdownSelect";
+
+export { InputText, InputPassword, InputToggle, InputSelect, DropdownSelect };
